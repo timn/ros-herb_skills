84d11ec84fc37eb3e60b2e9a14cc377b41b2901c
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-20 18:36:37 -0400

    Added .gitignore and initial skill space init

---
+++ src/herb_skills/init.lua
@@ -0,0 +1,15 @@
+
+----------------------------------------------------------------------------
+--  init.lua - Herb skills initialization file
+--
+--  Created: Fri Aug 20 18:25:22 2010 (at Intel Research, Pittsburgh)
+--  License: BSD
+--  Copyright  2010  Tim Niemueller [www.niemueller.de]
+--
+----------------------------------------------------------------------------
+
+require("fawkes.modinit")
+module(..., fawkes.modinit.register_all);
+
+skillenv = require("skiller.skillenv")
+require("skiller.skillhsm")
