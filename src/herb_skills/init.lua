ae3a40ee273fa9721dcf08e143d0c1f4edd7ede1
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-31 17:15:21 -0400

    Add HERB simple action

--- src/herb_skills/init.lua
+++ src/herb_skills/init.lua
@@ -16,5 +16,15 @@ local action_skill = require("skiller.ros.action_skill")
 require("skiller.skillhsm")
 
 --skillenv.use_skill("herb_skills.say")
+print("Initializing HERB skills")
 action_skill.debug = true
 action_skill.use("herb_skills.say", "/talker", "talkerapplet/Say")
+action_skill.use("herb_skills.stop_arms", "/hwctrl/arms/stop", "pr_msgs/Signal")
+action_skill.use("herb_skills.stop_segway", "/hwctrl/segway/stop", "pr_msgs/Signal")
+action_skill.use("herb_skills.resume_segway", "/hwctrl/segway/resume", "pr_msgs/Signal")
+action_skill.use("herb_skills.rotate_global", "/rails/rotate/global", "RobotOnRails/Rotate")
+action_skill.use("herb_skills.rotate_relative", "/rails/rotate/relative", "RobotOnRails/Rotate")
+action_skill.use("herb_skills.drive_forward", "/rails/drive/forward", "RobotOnRails/Drive")
+action_skill.use("herb_skills.goto", "/rails/goto", "RobotOnRails/Goto")
+action_skill.use("herb_skills.stop_rails", "/rails/stop", "pr_msgs/Signal")
+
